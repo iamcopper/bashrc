bf3e7b37c0e7859ac9fb71644a7361005dfb11a1
iamcopper <kangpan519@gmail.com>
2019-03-14 22:37:02 +0800

    Add default git config in bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -22,3 +22,14 @@ export PATH="${PATH}:${GOPATH}/bin"
 
 # gocomplete (bash complete for go)
 complete -C /home/kang.pan/go/bin/gocomplete go
+
+# Git Config
+if type git > /dev/null && ! git config --get user.name > /dev/null
+then
+	# default user name and email
+	git config --global user.name iamcopper
+	git config --global user.email kangpan519@gmail.com
+
+	# using SSH instead of HTTPS
+	git config --global url.git@:.insteadOf https://
+fi
