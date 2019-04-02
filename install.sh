051be2bcc62b37334447d652e85a02500ed6b2b0
iamcopper <kangpan519@gmail.com>
2019-04-02 12:56:16 +0800

    [install.sh] update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,59 @@
 #!/bin/bash
 
-# WARNING: unfinished
+function setup_vim()
+{
+	apt install -y vim ctags
+}
 
-# vim
-sudo apt install vim
-pip3 install pynvim
+function setup_git()
+{
+	apt install -y git
+}
 
-# git
-sudo apt install git
+function setup_go()
+{
+	local goversion=1.12.1
+	local gopackage=go${goversion}.linux-amd64.tar.gz
 
-# docker
+	wget -P /tmp https://dl.google.com/go/${gopackage} \
+	&& tar -C /usr/local -zxf /tmp/${gopackage} \
+	&& rm /tmp/${gopackage}
+}
 
-# go
+function setup_docker()
+{
+	apt install -y docker
+}
+
+function setup_ipmitool()
+{
+	apt install -y ipmitool
+}
+
+# Required root permission
+if [[ ${UID} -ne 0 ]]; then
+	echo "Error: Required root permission"
+	exit -1
+fi
+
+# Only support Debain like system
+if [[ ! -f /etc/apt/sources.list ]]; then
+	echo "Error: Not a Debain like system (debain, ubuntu ...)"
+	exit -1
+fi
+
+sed -i 's/archive.ubuntu.com/mirrors.163.com/g' \
+	/etc/apt/sources.list
+
+apt-get update && apt-get upgrade -y
+
+for (( i = 1; i <= $#; i++ ));
+do
+	case ${!i} in
+		vim       ) setup_vim;;
+		git       ) setup_git;;
+		go        ) setup_go;;
+		docker    ) setup_docker;;
+		ipmitool  ) setup_ipmitool;;
+	esac
+done
