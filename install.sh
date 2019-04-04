23bba504e44f04e7001c1057264b73f98eb96eb9
iamcopper <kangpan519@gmail.com>
2019-04-04 17:48:20 +0800

    [install.sh] fix some issues

--- install.sh
+++ install.sh
@@ -3,25 +3,31 @@
 function install_git()
 {
 	if type git &> /dev/null; then
-		echo "git has already installed."
-		return
+		echo ">>> GIT has already installed."
+	else
+		sudo apt install -y git
 	fi
 
-	sudo apt install -y git
+	if [[ -f ${HOME}/.gitconfig ]]; then
+		echo ">>> GIT has already configed."
+	else
+		git config --global user.name iamcopper
+		git config --global user.email kangpan519@gmail.com
 
-	git config --global user.name iamcopper
-	git config --global user.email kangpan519@gmail.com
+		git config --global push.default matching
+		git config --global core.editor vim
+	fi
 
-	git config --global push.default matching
-	git config --global core.editor vim
+	echo ">>> GIT installed finished."
 }
 
 function install_vim()
 {
-	local vim_path="~/repos/vimcfg"
+	local vim_path="${HOME}/repos/vimcfg"
 
 	if type vim &> /dev/null; then
-		return
+		echo ">>> VIM has already installed."
+		return 0
 	fi
 
 	if ! type git &> /dev/null; then
@@ -31,21 +37,21 @@ function install_vim()
 	sudo apt install -y vim ctags
 	
 	mkdir -p ${vim_path}
-	git clone  git@github.com:iamcopper/vimcfg.git ${vim_path}
-	bash ${vim_path}/install.sh
+	git clone  https://github.com/iamcopper/vimcfg.git ${vim_path}
+	cd ${vim_path} \
+	bash install.sh \
+	cd -
 
 	vim "+:PluginInstall" "+:qa"
+
+	echo ">>> VIM installed finished."
 }
 
 function install_go()
 {
 	if type go &> /dev/null; then
-		echo "go has already installed."
-		return
-	fi
-
-	if type vim &> /dev/null; then
-		install_vim
+		echo "GO has already installed."
+		return 0
 	fi
 
 	local goversion="1.12.1"
@@ -63,39 +69,47 @@ function install_go()
 	go get -u github.com/posener/complete/gocomplete
 	gocomplete -install
 
+	if type vim &> /dev/null; then
+		install_vim
+	fi
 	# install vim-go plugin stuff
 	vim "+:GoInstallBinaries"
+
+	echo ">>> GO installed finished."
 }
 
 function install_docker()
 {
 	if type docker &> /dev/null; then
-		echo "docker has already installed."
-		return
+		echo ">>> DOCKER has already installed."
+		return 0
 	fi
 
 	sudo apt install -y docker
 
 	# TODO
+	echo ">>> DOCKER installed finished."
 }
 
 function install_ipmitool()
 {
 	if type ipmitool &> /dev/null; then
-		echo "ipmitool has already installed."
-		return
+		echo ">>> IPMITOOL has already installed."
+		return 0
 	fi
 
 	sudo apt install -y ipmitool
+
+	echo ">>> IPMITOOL installed finished."
 }
 
 if [[ ${SHELL} != "/bin/bash" ]]; then
-	echo "Error: Not BASH Shell"
+	echo ">>> ERR: Not BASH Shell"
 	exit -1
 fi
 
 if [[ ! -f /etc/apt/sources.list ]]; then
-	echo "Error: Not a Debain like system (debain, ubuntu ...)"
+	echo ">>> ERR: Not a Debain like system (debain, ubuntu ...)"
 	exit -1
 fi
 
@@ -110,6 +124,4 @@ do
 		docker    ) install_docker;;
 		ipmitool  ) install_ipmitool;;
 	esac
-
-	. ${curdir}/setup_env.sh ${!i}
 done
