07407415bf9f2db4e126828613ca039f908d4ec2
iamcopper <kangpan519@gmail.com>
2019-05-06 14:44:28 +0800

    [setup_env.sh] add 4kpro environment settings.

--- setup_env.sh
+++ setup_env.sh
@@ -45,6 +45,12 @@ function set_ipmitool()
 	alias iol='ipmitool -I lanplus -U administrator -P advantech -H'
 }
 
+function set_nova4kpro()
+{
+	nova4kpro_path="${HOME}/4kpro/toolchain"
+	export PATH="${PATH}:${nova4kpro_path}/arm-linux-gnueabihf-5.4.1/bin/"
+}
+
 export LANG=C
 
 for (( i = 1; i <= $#; i++ ));
@@ -60,5 +66,6 @@ do
 			CURPATH=$(dirname ${BASH_SOURCE[0]})
 			. ${CURPATH}/setup_ipmicore_env.sh
 			;;
+		nova4kpro ) set_nova4kpro;;
 	esac
 done
